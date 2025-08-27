2e92f5b3bc695ca2c26d7bf649ebe846e61dffd8
Jhoel Andres <160800103+Jhoel777ar@users.noreply.github.com>
2025-08-26 20:02:14 -0400

    funcion de exportar y notificaciones emprendedor

--- app/Filament/Emprendedor/Resources/VentaResource/Pages/ListVentas.php
+++ app/Filament/Emprendedor/Resources/VentaResource/Pages/ListVentas.php
@@ -5,8 +5,23 @@ namespace App\Filament\Emprendedor\Resources\VentaResource\Pages;
 use App\Filament\Emprendedor\Resources\VentaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Exports\VentasExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListVentas extends ListRecords
 {
     protected static string $resource = VentaResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('exportar')
+                ->label('Exportar Excel')
+                ->color('success')
+                ->icon('heroicon-o-arrow-down-on-square-stack')
+                ->action(function () {
+                    return Excel::download(new VentasExport, 'ventas.xlsx');
+                }),
+        ];
+    }
 }
